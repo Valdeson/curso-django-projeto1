86e0b56dc6fb9893d484f735164d4d0ca93e289b
Valdeson <valdeson83@gmail.com>
2023-12-13 01:32:40 -0300

    Removido contatos e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-def contato(request):
-    return HttpResponse('Contato')
-
-def sobre(request):
-    return HttpResponse('Sobre')
+    return render(request, 'recipes/home.html', context = {'Nome':'Valdeson'})
